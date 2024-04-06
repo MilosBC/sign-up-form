b0fc32479b11e6c5d54f1454e0787928c1644536
Milos Stefanov <144391186+MilosBC@users.noreply.github.com>
2024-04-06 17:54:17 +0200

    Do validation for name, email, and phone number
    The validation for full name, email, and phone number is done

--- script.js
+++ script.js
@@ -8,10 +8,3 @@ for  (let i = 0; i < labels.length; i++) {
     labels[i].style.fontWeight = 'bold';
 } 
 
-const submitButton = document.querySelector('.account-creation');
-
-submitButton.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    alert("Well.....sorry to break it to you but you can't send the data right now. It was just a dummy form. The backend isn't set up yet, LOL" )
-})
